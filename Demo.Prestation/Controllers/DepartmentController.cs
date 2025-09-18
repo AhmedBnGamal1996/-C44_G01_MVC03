f56ee085b37af2114a80857fcdf24f5fd97c7eab
Ahmed Gamal <AhmedBnGamal2018@gmail.com>
2025-09-18 04:42:22 +0300

    Video 2 - 3 Create - Details

--- Demo.Prestation/Controllers/DepartmentController.cs
+++ Demo.Prestation/Controllers/DepartmentController.cs
@@ -2,17 +2,18 @@
 using Demo.DataAccess.Models;
 using Demo.BusinessLogic.Services.Classes;
 using Demo.BusinessLogic.Services.Interfaces;
+using Demo.BusinessLogic.DTOS;
 
 namespace Demo.Prestation.Controllers
 {
 
 
-    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    public class DepartmentController(IDepartmentService _departmentService , IWebHostEnvironment _env , ILogger<DepartmentController> _logger ) : Controller
     {
 
 
-
-
+        #region Index 
+        [HttpGet]
         public IActionResult Index()
         {
             var departments = _departmentService.GetAllDepartments();
@@ -20,8 +21,149 @@ namespace Demo.Prestation.Controllers
         }
 
 
+        #endregion
 
 
+
+        #region Create 
+
+        // Return View [ Form ] 
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto departmentDto)
+        {
+            if(ModelState.IsValid)      // Server Side Validation
+            {
+                try
+                {
+
+                   int result = _departmentService.AddDepartment(departmentDto);
+                    if (result > 0)
+                    { 
+
+                        return RedirectToAction(nameof(Index)); 
+
+
+                        //return View(viewName: "Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department can not be created");
+                    }
+
+
+                }
+
+
+                catch(Exception ex)
+                {
+
+                    if(_env.IsDevelopment())
+                    {
+                        _logger.LogError($"Department can not be created because : {ex.Message}");
+                    }
+                    else
+                    {
+                        _logger.LogError($"Department can not be created brcause {ex}");
+                        return View("ErrorView");
+                    }
+
+
+
+                }
+
+
+
+
+
+            }
+
+                return View(departmentDto); 
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+
+
+
+
+        #region Details
+
+
+
+        [HttpGet]
+
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue) return BadRequest();  // Error 400 
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if (department == null) return NotFound();  // Error 404 
+
+
+            return View(department);
+        }
+
+
+        #endregion
+
+        #region Edit
+
+
+
+        [HttpGet]
+
+        public IActionResult Edit (int? id )
+        {
+            if (id.HasValue) return BadRequest();
+            var department = _departmentService.GetDepartmentById(id.Value); 
+            if (department == null) return NotFound();
+
+
+            return View(department);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #endregion
 
 
 
